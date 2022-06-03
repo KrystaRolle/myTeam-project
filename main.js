d22ae0e336f1dae861ea1a406f748178b376204f
Noah Winkler <noahwinkler3700@icloud.com>
2022-06-03 14:28:20 -0500

    updated adding teammates names

--- main.js
+++ main.js
@@ -20,6 +20,15 @@ const myTeam = [
         pet: 'ferret'
     }
 ]
+const teammateNames = () => {
+    console.log('These are the teammates names.')
+    console.log('Krysta');
+    console.log('Tiffani');
+    console.log('Ben');
+    console.log('Noah');
+}
+
+teammateNames();
 
 const teamsBirthdays = (teamArray) => {
     for (const teamMember of teamArray) {
@@ -45,13 +54,3 @@ let teamPets = (team) => {
 
 let pets = teamPets(myTeam)
 console.log(pets)
-
-const teammateNames = () => {
-    console.log('These are the teammates names.')
-    console.log('Krysta');
-    console.log('Tiffani');
-    console.log('Ben');
-    console.log('Noah');
-}
-
-teammateNames();
