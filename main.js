d788fa15ee6962b7105792b73f61d73add08414c
Krysta Rolle <krystian.rolle@gmail.com>
2022-06-03 11:18:18 -0400

    myTeam project

---
+++ main.js
@@ -0,0 +1,22 @@
+const myTeam = [
+    {
+        name: 'Krysta',
+        birthday: '10/20/95',
+        pet: 'none',
+    },
+    {
+        name: 'Tiffani',
+        birthday: '10/11/88',
+        pet: 'dog & cat'
+    },
+    {
+        name: 'Ben',
+        birthday: '11/10/92',
+        pet: 'dog'
+    },
+    {
+        name: 'Noah',
+        birthday: '09/14/2000',
+        pet: 'ferret'
+    }
+]
